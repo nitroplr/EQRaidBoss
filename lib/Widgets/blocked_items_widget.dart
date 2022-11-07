02f397fc9e1b01774e92327915be8f5ca790ce2e
Benjamin LaSelle <laselb61@uwosh.edu>
2022-11-07 14:21:27 -0600

    Output blocked items to clipboard for sharing

--- lib/Widgets/blocked_items_widget.dart
+++ lib/Widgets/blocked_items_widget.dart
@@ -1,7 +1,9 @@
 import 'package:eq_raid_boss/Model/item_loot.dart';
 import 'package:eq_raid_boss/Providers/blocked_items_variables.dart';
 import 'package:eq_raid_boss/Providers/char_log_file_variables.dart';
+import 'package:eq_raid_boss/globals.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -39,21 +41,49 @@ class _BlockedItemsState extends ConsumerState<BlockedItems> {
     List<String> blockedItems = blockedItemsSet.toList();
     blockedItems.sort();
     return Scaffold(
-      body: ListView.builder(controller: ScrollController(),itemCount: blockedItems.length, itemBuilder:
-          (BuildContext context, int index) {
-        return TextButton(onPressed: () {}, child: Text(blockedItems[index]), onLongPress: () {
-          List<ItemLoot> allItems = ref.read(charLogFileVariableProvider).allItemLootsInRange;
-          allItems.forEach((itemLoot) {
-            if (itemLoot.item == blockedItems[index]) {
-              ref.read(charLogFileVariableProvider).itemLoots.add(itemLoot);
-            }
-          });
-          ref.read(charLogFileVariableProvider).itemLoots =ref.read(charLogFileVariableProvider).itemLoots;
-          blockedItems.removeWhere((element) => element == blockedItems[index]);
-          widget.prefs.setStringList('blockedItems', blockedItems);
-          blockedVariables.blockedItems = blockedItems;
-        },);
-      },),
+      body: Stack(
+        children: [
+          ListView.builder(
+            controller: ScrollController(),
+            itemCount: blockedItems.length,
+            itemBuilder: (BuildContext context, int index) {
+              return TextButton(
+                onPressed: () {},
+                child: Text(blockedItems[index]),
+                onLongPress: () {
+                  List<ItemLoot> allItems = ref.read(charLogFileVariableProvider).allItemLootsInRange;
+                  allItems.forEach((itemLoot) {
+                    if (itemLoot.item == blockedItems[index]) {
+                      ref.read(charLogFileVariableProvider).itemLoots.add(itemLoot);
+                    }
+                  });
+                  ref.read(charLogFileVariableProvider).itemLoots =
+                      ref.read(charLogFileVariableProvider).itemLoots;
+                  blockedItems.removeWhere((element) => element == blockedItems[index]);
+                  widget.prefs.setStringList('blockedItems', blockedItems);
+                  blockedVariables.blockedItems = blockedItems;
+                },
+              );
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Align(
+              alignment: Alignment.topRight,
+                child: IconButton(
+              icon: const Icon(Icons.copy),
+              onPressed: () {
+                StringBuffer blockedItemsString = StringBuffer();
+                for (var blockedItem in blockedItems) {
+                  blockedItemsString.writeln(blockedItem);
+                }
+                Clipboard.setData(ClipboardData(text: blockedItemsString.toString()));
+                showSnackBar(context: context, message: 'Blocked items copied to clipboard.');
+              },
+            )),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
@@ -66,10 +96,7 @@ class _BlockedItemsState extends ConsumerState<BlockedItems> {
                     children: [
                       TextField(
                         controller: blockListInputController,
-                        style: Theme
-                            .of(context)
-                            .textTheme
-                            .bodyText2,
+                        style: Theme.of(context).textTheme.bodyText2,
                         keyboardType: TextInputType.multiline,
                         maxLines: null,
                         decoration: const InputDecoration(
@@ -83,7 +110,7 @@ class _BlockedItemsState extends ConsumerState<BlockedItems> {
                             blocked.addAll(newBlocked);
                             List<String> sortedBlocked = blocked.toList();
                             sortedBlocked.sort();
-                            for(int i = 0; i < sortedBlocked.length; i++){
+                            for (int i = 0; i < sortedBlocked.length; i++) {
                               sortedBlocked[i] = sortedBlocked[i].toLowerCase().trim();
                             }
                             blockedVariables.blockedItems = sortedBlocked;
